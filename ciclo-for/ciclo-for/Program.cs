c89cdfc258e27c9bff7c9ee40bec68a34f315d26
Moisés Alejandro García Álvarez <mgarcia108@ucol.mx>
2019-08-24 16:56:37 -0500

    Ciclo For

--- ciclo-for/ciclo-for/Program.cs
+++ ciclo-for/ciclo-for/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 
-namespace ciclo_for
+namespace Tarea_For
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int x, y, z;
+			Console.WriteLine("De donde arrancamos el ciclo: ");
+			x = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Hasta donde terminamos el ciclo el ciclo:");
+			y = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("De cuanto en cuanto? ");
+			z = Convert.ToInt32(Console.ReadLine());
+
+			if (x < y)
+			{
+				for (int i = x; i <= y; i += z)
+				{
+					Console.WriteLine("i = " + i);
+				}
+			}
+			if (x > y)
+			{
+				for (int i = x; i >= y; i -= z)
+				{
+					Console.WriteLine("i = " + i);
+				}
+			}
 		}
 	}
 }
